137e8569ab0df8c443d72016cbfae3650271279d
marciadpmell <mdepmell@gmail.com>
2022-06-09 20:08:01 -0300

    estados insta4

--- modulo2/estados-insta4/src/App.js
+++ modulo2/estados-insta4/src/App.js
@@ -9,25 +9,133 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+const FormContainer = styled.form`
+  width: 280px;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0 10px 20px;
+  box-shadow: 0 1px 5px #0e0e0e;
+  margin: 20px 0;
+
+  input {
+    width: 80%;
+    height: 40px;
+    padding: 0 10px;
+    margin: 0 0 20px;
+  }
+
+  button {
+    width: 60%;
+    height: 40px;
+    border: 0;
+    background-color: #0e0e0e;
+    color: #FFFFFF;
+    border-radius: 5px;
+  }
+`
+
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+        nome: "Márcia",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150"
+      },
+      {
+        nome: "Paulinha",
+        fotoUsuario: "https://picsum.photos/45/50",
+        fotoPost: "https://picsum.photos/157/150"
+      },
+      {
+        nome: "Joãozinho",
+        fotoUsuario: "https://picsum.photos/48/50",
+        fotoPost: "https://picsum.photos/198/150"
+      },
+      {
+        nome: "Pedrinho",
+        fotoUsuario: "https://picsum.photos/46/50",
+        fotoPost: "https://picsum.photos/196/150"
+      }
+    ],
+
+    valorInputPessoa: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+  };
+  
+  adicionaPessoa = (event) => {
+    event.preventDefault()
+    const { posts, valorInputFotoPost, valorInputFotoUsuario, valorInputPessoa } = this.state;
+
+    const novaPost = {
+      nome: valorInputPessoa,
+      fotoUsuario: valorInputFotoUsuario,
+      fotoPost: valorInputFotoPost
+    };
+
+    const copiaPosts = [...posts, novaPost]
+
+    this.setState({ posts: copiaPosts });
+
+  };
+
+  onChangeInputPessoa = (event) => {
+
+    this.setState({ valorInputPessoa: event.target.value });
+  };
+
+  onChangeInputFotoUsuario = (event) => {
+
+    this.setState({ valorInputFotoUsuario: event.target.value });
+
+  };
+
+  onChangeInputFotoPost = (event) => {
+
+    this.setState({ valorInputFotoPost: event.target.value });
+  };
+
+
   render() {
+    const { posts } = this.state;
+
+    const listaPosts = (
+      posts.map((post, index) => {
+        return <Post
+          key={index}
+          nomeUsuario={post.nome}
+          fotoUsuario={post.fotoUsuario}
+          fotoPost={post.fotoPost}
+        />
+      })
+    )
+
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />        
-        <Post
-          nomeUsuario={'Márcia'}
-          fotoUsuario={'https://picsum.photos/49/50'}
-          fotoPost={'https://picsum.photos/199/150'}
-        />
-        <Post
-          nomeUsuario={'Joãozinho'}
-          fotoUsuario={'https://picsum.photos/48/50'}
-          fotoPost={'https://picsum.photos/198/150'}
-        />
+        <FormContainer onSubmit={this.adicionaPessoa} >
+          <h4>Formulário de criação de post</h4>
+          <input
+            value={this.state.valorInputPessoa}
+            onChange={this.onChangeInputPessoa}
+            placeholder="Nome do usuário"
+          />
+          <input
+            value={this.state.valorInputFotoUsuario}
+            onChange={this.onChangeInputFotoUsuario}
+            placeholder="Foto do usuário"
+          />
+          <input
+            value={this.state.valorInputFotoPost}
+            onChange={this.onChangeInputFotoPost}
+            placeholder="Foto do Post"
+          />
+          <button type='submit'>Criar</button>
+        </FormContainer>
+        {listaPosts}
       </MainContainer>
     );
   }
