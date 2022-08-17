01359b999ad63e183272a9eaff4640f8e5bd4fbc
marciadpmell <mdepmell@gmail.com>
2022-08-16 23:10:39 -0300

    aprofundamento express

---
+++ modulo4/aprofundamento-express/src/lists/todo-list.ts
@@ -0,0 +1,24 @@
+// typings
+import { Todo } from "../typings";
+
+// exercicio 3
+export const todoList: Todo[] = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+    }
+]
