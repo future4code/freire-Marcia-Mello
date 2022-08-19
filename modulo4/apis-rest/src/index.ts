20780868e9f95fdf7a055fbb1bb4b5044a869193
marciadpmell <mdepmell@gmail.com>
2022-08-19 00:18:10 -0300

    apis-rest

--- modulo4/apis-rest/src/index.ts
+++ modulo4/apis-rest/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { AddressInfo } from "net";
+import { User } from "./typings";
+import { userList } from "./usersList";
 
 const app = express();
 
@@ -13,3 +15,38 @@ const server = app.listen(process.env.PORT || 3003, () => {
         console.error('Failure upon starting server.');
     }
 });
+// Exercicio 1
+
+app.get("/users", (req: Request, res: Response) => {
+    try {
+        res.status(200).send("user")
+    } catch (error: any) {
+        res.status(400).send({
+            message: error.message
+        })
+    }
+})
+// a = Get
+// b = "/users"
+
+// Exercicio 4
+app.post("/createUsers", (req: Request, res: Response) => {
+    try {
+
+        const user: User = {
+            id: req.body.id,
+            name: req.body.name,
+            email: req.body.email,
+            type: req.body.type,
+            age: req.body.type
+        }
+
+        userList.push(user);
+
+        res.status(200).send(userList)
+    } catch (error: any) {
+        res.status(400).send({
+            message: error.message
+        })
+    }
+})
--- modulo4/apis-rest/src/usersList.ts
+++ modulo4/apis-rest/src/usersList.ts
@@ -1,44 +1,47 @@
-gilet users = [
+// Exercicio 2a
+import { User, USER_TYPE } from './typings'
+
+export let userList: User[] = [
     {
         id: 1,
         name: "Alice",
         email: "alice@email.com",
-        type: "ADMIN",
+        type: USER_TYPE.ADMIN,
         age: 12
     },
     {
         id: 2,
         name: "Bob",
         email: "bob@email.com",
-        type: "NORMAL",
+        type: USER_TYPE.NORMAL,
         age: 36
     },
     {
         id: 3,
         name: "Coragem",
         email: "coragem@email.com",
-        type: "NORMAL",
+        type: USER_TYPE.NORMAL,
         age: 21
     },
     {
         id: 4,
         name: "Dory",
         email: "dory@email.com",
-        type: "NORMAL",
+        type: USER_TYPE.NORMAL,
         age: 17
     },
     {
         id: 5,
         name: "Elsa",
         email: "elsa@email.com",
-        type: "ADMIN",
+        type: USER_TYPE.ADMIN,
         age: 17
     },
     {
         id: 6,
         name: "Fred",
         email: "fred@email.com",
-        type: "ADMIN",
+        type: USER_TYPE.ADMIN,
         age: 60
     }
 ]
